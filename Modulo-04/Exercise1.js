fe71051cd72fa5da13d89793e4ec1d0af68ffb47
Bruno Teixeira <bruno.teixeira.001@acad.pucrs.br>
2019-01-04 21:26:43 -0200

    Todos os modulos do curso JavaScript starter finalizados

--- Modulo-04/Exercise1.js
+++ Modulo-04/Exercise1.js
@@ -1,10 +1,11 @@
+var maiorDeIdade = "Maior de 18 anos de idade";
+var menorDeIdade = "Menor de 18 anos de idade";
+
 var checaIdade = function(idade) {
     return new Promise(function(resolve, reject) {
-        if(idade >= 18){
-            resolve('Maior que 18 anos de idade.');
-        }else{
-            reject('Menor que 18 anos de idade');
-        }
+        setTimeout( function() {
+            return idade >= 18 ? resolve() : reject();
+        }, 2000);
 })};
 
 checaIdade(15)
--- Modulo-04/Exercise3.js
+++ Modulo-04/Exercise3.js
@@ -14,6 +14,8 @@ bodyElement.appendChild(buttonElement);
 bodyElement.appendChild(listElement);
 
 function showRepos(repos){
+listElement.innerHTML = '';
+
     for(repo of repos) {
         const textItem = document.createTextNode(repo.name);
         const listItem = document.createElement('li');
@@ -23,13 +25,40 @@ function showRepos(repos){
     }
 }
 
+function showLoading(){
+    listElement.innerHTML = '';
+
+    var loadText = document.createTextNode('Carregando....');
+    var loadLabel = document.createElement('li');
+
+    loadLabel.appendChild(loadText);
+    listElement.appendChild(loadLabel);
+}
+
+function showError(){
+    listElement.innerHTML = '';
+
+    var errorText = document.createTextNode('Nao foi possivel encontrar o usuario ' + getUser());
+    var errorLabel = document.createElement('li');
+
+    errorLabel.style.color = '#FF5555';
+
+    errorLabel.appendChild(errorText);
+    listElement.appendChild(errorLabel);
+}
+
 function getRepos(){
     if( !getUser() ) return;
+
+    showLoading();
 
     axios.get('https://api.github.com/users/' + getUser() + '/repos')
     .then(function (response) {
         showRepos(response.data);
-    }) 
+    })
+    .catch(function () {
+        showError();
+    });
 }
 
 function getUser(){
